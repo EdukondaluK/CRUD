4b753ece41c37135810e20d2c9aa77e28d98e0cb
Edukondalu <edukondalu.eee@gmail.com>
2021-04-28 16:58:00 +0530

    Update config-default.py

--- config-default.py
+++ config-default.py
@@ -1,12 +1,6 @@
-# Tera data
-API_HOST = 'https://delltdprod2-vp2.us.dell.com:1443/tdrest/systems/tddev_sls_insights/queries'
-API_USERNAME = 'SERVICE_REST_SLS_INSIGHTS'
-API_PASSWORD = 'Donotsharewithanyone_321'
-
-
 # Postgres
-DB_HOST = 'atspgsdev001.us.dell.com'
+DB_HOST = 'xxxx'
 DB_PORT = '9432'
-DB_DATABASE = 'atsd'
-DB_USERNAME = 'salesinsights_all'
-DB_PASSWORD = 'salesinsights_all813'
+DB_DATABASE = 'xx'
+DB_USERNAME = 'xxxx'
+DB_PASSWORD = 'xxxxxx'
